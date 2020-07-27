2b40e635c813a6340fb213822764cbad53cdff38
wangxfh <xiaofeix.wang@gmail.com>
2020-07-27 17:01:27 +0800

    init base project

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	klogv2 "k8s.io/klog/v2"
 
-	"github.com/gin-gonic/gin"
+	"blog_service/internal/routers"
 )
 
-func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
+const HTTPPort = 8080
 
-	if err := r.Run(); err != nil {
-		klogv2.Infof("start gin service error: %v", err)
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:              fmt.Sprintf(":%d", HTTPPort),
+		Handler:           router,
+		TLSConfig:         nil,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       10 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		klogv2.Infof("start http server error: %v", err)
 		os.Exit(1)
 	}
 }
