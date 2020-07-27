e8f9b00b210d7f460a3736861e930a22fe1f3896
wangxfh <xiaofeix.wang@gmail.com>
2020-07-27 18:43:17 +0800

    init base project

--- main.go
+++ main.go
@@ -1,26 +1,69 @@
 package main
 
 import (
+	"blog_service/internal/model"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
+
 	klogv2 "k8s.io/klog/v2"
 
+	"blog_service/global"
 	"blog_service/internal/routers"
+	"blog_service/pkg/setting"
 )
 
-const HTTPPort = 8080
+func init() {
+	if err := setupSetting(); err != nil {
+		klogv2.Fatalf("init setup setting error: %v", err)
+	}
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	if err = setting.ReadSection("Server", &global.ServerSetting); err != nil {
+		return err
+	}
+	if err = setting.ReadSection("App", &global.AppSetting); err != nil {
+		return err
+	}
+	if err = setting.ReadSection("Database", &global.DatabaseSetting); err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	return nil
+}
 
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
+
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:              fmt.Sprintf(":%d", HTTPPort),
+		Addr:              fmt.Sprintf(":%s", global.ServerSetting.HttpPort),
 		Handler:           router,
 		TLSConfig:         nil,
-		ReadTimeout:       10 * time.Second,
-		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       global.ServerSetting.ReadTimeout,
+		ReadHeaderTimeout: global.ServerSetting.WriteTimeout,
 		WriteTimeout:      10 * time.Second,
 		IdleTimeout:       10 * time.Second,
 		MaxHeaderBytes:    1 << 20,
